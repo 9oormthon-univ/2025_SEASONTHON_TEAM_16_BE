84e696bff2dda8bcebf881c604a5d91f0a550821
KTH1007 <taeheon@gimtaeheon-ui-MacBookPro.local>
2025-08-29 23:49:20 +0900

    init

---
+++ src/main/java/com/seasonthon/YEIN/YeinApplication.java
@@ -0,0 +1,13 @@
+package com.seasonthon.YEIN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seasonthon/YEIN/YeinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seasonthon.YEIN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
