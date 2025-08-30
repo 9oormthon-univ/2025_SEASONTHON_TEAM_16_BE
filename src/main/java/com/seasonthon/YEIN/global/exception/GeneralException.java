b2f72a362fa9fb0b67c914978d38ef56a4a13839
KTH1007 <taeheon@gimtaeheon-ui-MacBookPro.local>
2025-08-30 10:32:34 +0900

    feat/#5: 공통 응답 및 예외 처리 로직 구현

---
+++ src/main/java/com/seasonthon/YEIN/global/exception/GeneralException.java
@@ -0,0 +1,14 @@
+package com.seasonthon.YEIN.global.exception;
+
+import com.seasonthon.YEIN.global.code.BaseErrorCode;
+import lombok.Getter;
+
+@Getter
+public class GeneralException extends RuntimeException {
+    private final BaseErrorCode code;
+
+    public GeneralException(BaseErrorCode code) {
+        super(code.getMessage());
+        this.code = code;
+    }
+}
