474b0cd543721081bdc1ab11f47177dcab167bb4
KTH1007 <taeheon@gimtaeheon-ui-MacBookPro.local>
2025-08-30 16:50:24 +0900

    feat/#15: S3를 이용한 파일 업로드 설정

--- src/main/java/com/seasonthon/YEIN/global/code/status/ErrorStatus.java
+++ src/main/java/com/seasonthon/YEIN/global/code/status/ErrorStatus.java
@@ -13,6 +13,7 @@ public enum ErrorStatus implements BaseErrorCode {
     VALIDATION_FAILED(HttpStatus.BAD_REQUEST, "VALID4001", "입력값 유효성 검증에 실패했습니다."),
     MISSING_PARAMETER(HttpStatus.BAD_REQUEST, "VALID4002", "필수 파라미터가 누락되었습니다."),
     TYPE_MISMATCH(HttpStatus.BAD_REQUEST, "VALID4003", "파라미터 타입이 올바르지 않습니다."),
+    FILE_IS_EMPTY(HttpStatus.BAD_REQUEST, "FILE4004", "업로드할 파일이 없습니다."),
 
     // 401 Unauthorized
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "AUTH401", "인증이 필요합니다."),
@@ -41,6 +42,8 @@ public enum ErrorStatus implements BaseErrorCode {
     // 500 Internal Server Error
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러, 관리자에게 문의 바랍니다."),
     DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB5003", "데이터베이스 처리 중 오류가 발생했습니다."),
+    S3_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "S35001", "파일 업로드 중 오류가 발생했습니다."),
+    S3_DELETE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "S35002", "파일 삭제 중 오류가 발생했습니다."),
 
     // 502, 503, 504
     BAD_GATEWAY(HttpStatus.BAD_GATEWAY, "COMMON502", "불완전한 게이트웨이 응답을 받았습니다."),
