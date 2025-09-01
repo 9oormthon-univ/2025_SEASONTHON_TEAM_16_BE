10664579ec146c1fc87d747f8a8b9e752677f6f6
KTH1007 <taeheon@gimtaeheon-ui-MacBookPro.local>
2025-09-02 00:29:23 +0900

    feat/#30: 게시글 조회 N + 1 문제 해결

--- src/main/java/com/seasonthon/YEIN/post/domain/repository/PostRepository.java
+++ src/main/java/com/seasonthon/YEIN/post/domain/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.seasonthon.YEIN.post.domain.repository;
 import com.seasonthon.YEIN.post.domain.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -10,6 +11,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    @EntityGraph(attributePaths = "user")
     @Query("SELECT p FROM Post p WHERE " +
             "(:keyword IS NULL OR :keyword = '' OR " +
             "LOWER(p.quote) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
@@ -21,6 +23,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
             "p.createdAt DESC, p.id DESC")
     Page<Post> findPostsWithFilter(@Param("keyword") String keyword, @Param("sortBy") String sortBy, Pageable pageable);
 
+    @EntityGraph(attributePaths = "user")
     @Query("SELECT p FROM Post p WHERE p.user.id = :userId AND " +
             "(:keyword IS NULL OR :keyword = '' OR " +
             "LOWER(p.quote) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
